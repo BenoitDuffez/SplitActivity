db718f559169ff22ff48b89fa2184c94e850fc74
Benoit Duffez <benoit@bicou.net>
2013-08-12 11:38:25 +0200

    updated Samples with subactvity

---
+++ Samples/src/net/bicou/android/splitactivity/samples/SubActivity.java
@@ -0,0 +1,15 @@
+package net.bicou.android.splitactivity.samples;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+
+/**
+ * Created by bicou on 12/08/13.
+ */
+public class SubActivity extends SherlockActivity {
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.subactivity);
+	}
+}
